08230df7a1ffa9715c2692d677c92f22e2960a6c
geicibarham <geicianecosta43@gmail.com>
2022-04-07 09:13:28 -0400

    created job model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,31 @@
 const Owner = require('./owner');
 const Walker = require('./walker');
-const Pets = require('./pets')
+const Pets = require('./pets');
+const Job = require('./Job');
 
+Owner.hasMany(Pets, {
+    foreignKey: 'animal_id'
+  });
+  
+  Pets.belongsTo(Owner, {
+    foreignKey: 'owner_id',
+    onDelete: 'CASCADE'
+  });
+  
+  Owner.hasMany(Job,{
+    foreignKey: 'owner_id',
+  })
+  
+  Walker.hasMany(Job, {
+      foreignKey: 'walker_id'
+    });
+  
+  Job.belongsTo(Walker, {
+    foreignKey: 'walker_id'
+  });
+  
+  Job.belongsTo(Owner, {
+    foreignKey: 'owner_id'
+  });
 
-module.exports = { Walker, Owner, Pets };
+module.exports = { Walker, Owner, Pets, Job };
--- models/pets.js
+++ models/pets.js
@@ -1,44 +1,48 @@
 const { Model, DataTypes, INTEGER } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Pets extends Model {}
+class Pets extends Model { }
 
 // define table columns and configuration
 Pets.init(
-    {
-      // TABLE COLUMN DEFINITIONS 
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      pet_name: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      owner_id: {
-         type: DataTypes.INTEGER,
-         allowNull: false
-      },
-      pet_type: {
-          type: DataTypes.STRING,
-          allowNull: false
-      },
-      description: {
-         type: DataTypes.STRING,
-         allowNull: false
+  {
+    // TABLE COLUMN DEFINITIONS 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    pet_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    owner_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'owner',
+        key: 'id'
       }
     },
-    {
-     
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'pets'
+    pet_type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-  );
-  
+  },
+  {
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'pets'
+  }
+);
+
 
 module.exports = Pets;
