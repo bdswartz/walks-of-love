2030973960c28af4bd08377e76d076e33153458d
JRM <johnrhmartin@gmail.com>
2022-04-06 22:39:05 -0400

    Added /api/pet-routes.js and updated /api/index.js

--- controllers/api/pet-routes.js
+++ controllers/api/pet-routes.js
@@ -2,30 +2,30 @@ const router = require('express').Router();
 // const withAuth = require('../../utils/auth');
 const { Pets } = require('../../models');
 
-/// GET /api/pet
+/// GET /api/pets
 router.get('/', (req, res) => {
     // Acces our Pets model and run .findAll() method
     Pets.findAll()
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbPetsData => res.json(dbPetsData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-// GET /api/pet/id
+// GET /api/pets/id
 router.get('/:id', (req, res) => {
     Pets.findOne({
         where: {
           id: req.params.id
         }
       })
-        .then(dbUserData => {
-          if (!dbUserData) {
+        .then(dbPetsData => {
+          if (!dbPetsData) {
             res.status(404).json({ message: 'There was no pet found with this id.' });
             return;
           }
-          res.json(dbUserData);
+          res.json(dbPetsData);
         })
         .catch(err => {
           console.log(err);
@@ -33,7 +33,7 @@ router.get('/:id', (req, res) => {
         });
 });
 
-// POST /api/users
+// POST /api/Pets
 router.post('/', (req, res) => {
     Pets.create({
         id: req.body.id,
@@ -42,17 +42,17 @@ router.post('/', (req, res) => {
         pet_type: req.body.pet_type,
         description: req.body.description
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbPetsData => res.json(dbPetsData))
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });
 });
 
-// PUT /api/users/1
+// PUT /api/Pets/1
 router.put('/:id', (req, res) => {});
 
-// DELETE /api/users/1
+// DELETE /api/Pets/1
 router.delete('/:id', (req, res) => {});
 
 module.exports = router;
