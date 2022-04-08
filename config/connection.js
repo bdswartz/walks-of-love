ccd01507b0f5270037724b68ccd8361e77835efd
david curtis <heythereimdavidcurtis@gmail.com>
2022-04-08 14:48:34 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -9,11 +9,11 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
     host: "127.0.0.1",
-    username: 'root',
-    password: 'password',
-    database: 'walks_of_love_db',
+    // username: "root",
+    // password: "password",
+    // database: "walks_of_love_db",
     dialect: "mysql",
     port: 3306,
   });
