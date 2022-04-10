f683e285139736292322f909cf813cea24b8141d
geicibarham <geicianecosta43@gmail.com>
2022-04-09 20:43:42 -0400

    finally got something to work

--- public/javascript/create-account.js
+++ public/javascript/create-account.js
@@ -1,58 +1,45 @@
-async function createPostHandler(event) {
-    event.preventDefault();
+async function createAccountHandler(event) {
+  event.preventDefault();
+
+  const first_name = document.querySelector('input[name="first"]').value.trim();
+  const last_name = document.querySelector('input[name="last"]').value.trim();
+  const email = document.querySelector('input[name="email"]').value.trim();
+  const password = document.querySelector('input[name="password"]').value.trim();
+
+
+
+  if ((email, first_name, last_name, password)) {
+
+   if(document.getElementById('walk').checked) {
   
-    const payStr = document.querySelector('input[name="pay"]').value.trim();
-    const pay = parseInt(payStr);
-  
-    const dateStart = document.getElementById("date-start").value;
-    const hourStart = document.getElementById("hour-start").value;
-    const timeframe = dateStart + "T" + hourStart + "Z";
-  
-    const locationStr = document
-      .querySelector('input[name="location"]')
-      .value.trim();
-    const location = parseInt(locationStr);
-  
-    var visitType = document.getElementById("visit-type");
-    var selectedValue = visitType.options[visitType.selectedIndex].value;
-    if (selectedValue == "Walk") {
-      walk = true;
-      check_in = false;
-    } else {
-      walk = false;
-      check_in = true;
-    }
-  
-    if ((pay, location, timeframe)) {
-      const response = await fetch("/api/jobs", {
-        method: "POST",
-        body: JSON.stringify({
-          pay,
-          check_in,
-          walk,
-          timeframe,
-          location,
-          completed: false,
-          owner_id: 1,
-          walker_id: null,
-          animal_id: 1,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-  
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        alert(response.statusText);
+    const response = await fetch("/api/walkers", {
+      method: "POST",
+      body: JSON.stringify({
+        first_name,
+        last_name,
+        email,
+        password,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
       }
+    });
+
+    if (response.ok) {
+      // document.location.reload();
+      document.location.replace('/jobs');
+    } else {
+      alert(response.statusText);
     }
   }
-  
-  document
-    .querySelector(".create-job-form")
-    .addEventListener("submit", createPostHandler);
-  
+}
+}
 
-    console.log("test")
+
+document
+  .querySelector(".create-account-form")
+  .addEventListener('submit', createAccountHandler);
+
+
+// console.log("test")
+// console.log("test")
