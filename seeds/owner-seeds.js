e2990d2b4993874546d6f139cc9752bf8d223270
JRM <johnrhmartin@gmail.com>
2022-04-10 13:26:53 -0400

    added my owner and walker seed data to the corresponding seed files.

---
+++ seeds/owner-seeds.js
@@ -0,0 +1,26 @@
+const { Owner } = require('../models');
+
+const ownerData = [
+    {
+        id: "SP2Q6C1P00N04NBE5HF2H81R52S3VD04V4BCBW59K",
+        first_name: 'Rebekkah',
+        last_name: 'Owner',
+        email: 'email',
+        password: 'password'
+        
+    },
+    {
+        id: "SP29AZWNBFXEHJGBQ2BMQ71W8R79DCA3NZQ7QJ367",
+        first_name: 'John',
+        last_name: 'Owner',
+        email: 'email',
+        password: 'password'
+        
+    }
+
+];
+
+
+const seedOwner = () => Owner.bulkCreate(ownerData);
+
+module.exports = seedOwner;
---
+++ seeds/walker-seeds.js
@@ -0,0 +1,26 @@
+const { Walker } = require('../models');
+
+const walkerData = [
+    {
+        id: "SP20XG0Q9NW6N2HT6JZHXX6S5XZGS01P3QSVTTCMV",
+        first_name: 'Rebekkah',
+        last_name: 'Walker',
+        email: 'email',
+        password: 'password'
+        
+    },
+    {
+        id: "SP206YJ9G14C3FD0JZE3C92KZ3V35JVNVW93Q0VKQ",
+        first_name: 'John',
+        last_name: 'Walker',
+        email: 'email',
+        password: 'password'
+        
+    }
+];
+
+
+    const seedWalker = () => Walker.bulkCreate(walkerData);
+
+    module.exports = seedWalker;
+    
