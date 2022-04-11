96951397a4030379dc7b60ad89cc044dd56586df
david curtis <heythereimdavidcurtis@gmail.com>
2022-04-10 22:13:13 -0400

    changed DB_PASS to DB_PW

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_PW,
     {
       host: "localhost",
       // username: "root",
