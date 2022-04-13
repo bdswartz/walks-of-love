ad80d13f9339022ca4f3a15b4309448c45108937
geicibarham <geicianecosta43@gmail.com>
2022-04-13 19:21:24 -0400

    anyone can see jobs now

--- public/javascript/accept-job.js
+++ public/javascript/accept-job.js
@@ -8,8 +8,13 @@ async function acceptJobHandler(event) {
       "Content-Type": "application/json",
     },
   });
+
   const walker_id = await res.json();
   console.log(walker_id.walkerId);
+
+  if(walker_id.walkerId ==1){
+    window.alert("a")
+      }
 
   const response = await fetch(`/api/jobs/accept/${id}`, {
     method: "PUT",
@@ -20,9 +25,14 @@ async function acceptJobHandler(event) {
       "Content-Type": "application/json",
     },
   });
+
+
   if (response.ok) {
     document.location.reload();
-  } else {
+  } 
+
+ 
+  else {
     alert(response.statusText);
   }
 }
